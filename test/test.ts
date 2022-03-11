b4c1912ed185aa2c3d6235e6c57bc1c4abca0ed3
Ferotiq <evanrayweigel@outlook.com>
2022-03-10 21:42:08 -0600

    Delete test.ts

--- test/test.ts
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import { MonkeytypeClient } from "../src/index";
-
-import { apeKey } from "./config.json";
-
-const client = new MonkeytypeClient(apeKey);
-
-client.users.getPersonalBests("time", 15).then(console.log);
