17b4b23f85a534e439751f015f10f6db01ca2c0a
Ferotiq <evanrayweigel@outlook.com>
2022-03-19 12:01:54 -0500

    slightly better testing

--- test/test.js
+++ test/test.js
@@ -4,10 +4,19 @@ const { MonkeytypeClient } = require("../dist/index.js");
 
 const { apeKey } = require("./config.json");
 
+const { writeFileSync } = require("fs");
+
 const client = new MonkeytypeClient(apeKey);
 
 // TODO Add proper unit testing
-client.users.getPersonalBests("time").then(console.log);
-client.users.getPersonalBests("time", "60").then(console.log);
-client.leaderboards.get("english", "time", "60").then(console.log);
-client.leaderboards.getRank("english", "time", "60").then(console.log);
+
+(async () => {
+  const [pbTime, pbTime60, lb, lbRank] = await Promise.all([
+    client.users.getPersonalBests("time"),
+    client.users.getPersonalBests("time", "60"),
+    client.leaderboards.get("english", "time", "60"),
+    client.leaderboards.getRank("english", "time", "60"),
+  ]);
+
+  writeFileSync("./test.json", JSON.stringify({ pbTime, pbTime60, lb, lbRank }, null, 2));
+})();
