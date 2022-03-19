624a33ff10136ad7d6dc34afc13898aa5f5221ae
Evan <64989416+Ferotiq@users.noreply.github.com>
2022-03-19 11:26:31 -0500

    Improve readability

--- src/endpoints/users.ts
+++ src/endpoints/users.ts
@@ -16,10 +16,15 @@ export class UsersEndpoints {
     mode: M,
     mode2?: Mode2<M>,
   ): Promise<ApiResponse<PersonalBests[M]>> {
-    const query = {
-      mode,
-      ...(mode2 !== undefined && { mode2: <string>mode2 }),
+    const mode2String = mode2?.toString() ?? undefined;
+      
+    const query: { mode: typeof mode, mode2?: typeof mode2String } = {
+      mode
     };
+      
+    if (mode2String !== undefined) {
+      query.mode2 = mode2String; 
+    }
 
     return this.httpClient.get(`${BASE_PATH}/personalBests`, { query });
   }
